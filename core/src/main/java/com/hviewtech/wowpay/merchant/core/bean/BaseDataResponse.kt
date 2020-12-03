fabda5a4af0896daec63c0f745be25dc7fec9f6a
adness <2659866365@qq.com>
2020-12-03 14:33:04 +0800

    build  修改

---
+++ core/src/main/java/com/hviewtech/wowpay/merchant/core/bean/BaseDataResponse.kt
@@ -0,0 +1,3 @@
+package com.hviewtech.wowpay.merchant.core.bean
+
+class BaseDataResponse<T>(val data: T?) : BaseResponse()
---
+++ core/src/main/java/com/hviewtech/wowpay/merchant/core/bean/BaseResponse.kt
@@ -0,0 +1,6 @@
+package com.hviewtech.wowpay.merchant.core.bean
+
+open class BaseResponse {
+    var code = 0
+    var msg: String? = null
+}
